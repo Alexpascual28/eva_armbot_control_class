4da62aff73b5e8eef4778af2f91e373ae4d03316
Alexpascual28 <apsr2809@gmail.com>
2024-04-09 15:28:08 +0100

    Updated armbot class

--- armbot.py
+++ armbot.py
@@ -13,6 +13,11 @@ from evasdk import Eva
 import time
 import cv2
 from aravis import Camera
+import threading
+import numpy as np
+import csv
+import os
+
 
 # 2. Initialize your kinematics functions and variables including the constants (dimensions, etc.) for the EVA robot arm.
 class ArmBot:
@@ -122,6 +127,12 @@ class ArmBot:
         print("\n")
 
         print("Done.\n")
+
+        self.current_image = None # Instantiates variable to store current current image
+
+        # Create CSV file with default hsv values if none exists already, and read those values to possible colours
+        self.hsv_filename = 'hsv_colour_values.csv'
+        self.current_hsv_values = self.read_hsv_values(self.hsv_filename)
         
         return cam
 
@@ -187,8 +198,25 @@ class ArmBot:
             self.eva.gpio_set('ee_d1', True)
             self.eva.gpio_set('ee_d0', False)
 
+    # Start continuous image acquisition
+    def start_image_acquisition(self, time_period = 86400, show_feed = True):
+        self.camera_thread = threading.Thread(target=self.show_camera_view, args=(time_period, show_feed))
+        self.camera_thread.start()
+
+    # Returns current image
+    def get_current_image(self):
+        return self.current_image
+    
+    # Displays image
+    def show_image(self, frame_name, image):
+        cv2.imshow(frame_name, image)
+
+    # Stops image acquisition thread
+    def stop_image_acquisition(self):
+        self.camera_thread.join()
+
     # Shows continuous camera view
-    def show_camera_view(self, time_period):
+    def show_camera_view(self, time_period = 60, show_feed = True):
         try:
             # Start the camera
             self.cam.start_acquisition_continuous()
@@ -210,10 +238,10 @@ class ArmBot:
                 
                 if not 0 in frame.shape:
                     # Convert to standard RGB format
-                    image = cv2.cvtColor(frame, cv2.COLOR_BayerRG2RGB)
+                    self.current_image = cv2.cvtColor(frame, cv2.COLOR_BayerRG2RGB)
             
                     # Show the image and wait a short time with:
-                    cv2.imshow("capture", image)
+                    if show_feed == True: cv2.imshow("capture", self.current_image)
                     cv2.waitKey(delay)
                     
         except KeyboardInterrupt:
@@ -225,6 +253,7 @@ class ArmBot:
             self.cam.shutdown()
             print("Camera Off")
 
+    # Takes one picture
     def take_picture(self):
         try:
             # Start the camera
@@ -250,3 +279,181 @@ class ArmBot:
             self.cam.shutdown()
             print("Camera Off")
 
+    # Detect colour from given image
+    def detect_colour(self, image, colour_name, frame_name="colour frame", image_format="hsv", show_frame = True):
+            blurred_image = self.blurring(image)
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+            if colour_name in self.current_hsv_values:
+                hsv_min = tuple(self.current_hsv_values.get(colour_name)[0])
+                hsv_max = tuple(self.current_hsv_values.get(colour_name)[1])
+
+                mask=cv2.inRange(hsv,hsv_min,hsv_max)
+                masked_image = cv2.bitwise_and(image,image,mask=mask)
+
+                self.frame_count += 1
+                average_fps = self.frame_count / ( time.time() - self.start_time )
+                cv2.putText(masked_image,"%2.1f fps" % average_fps, (50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,255),2,cv2.LINE_AA)
+
+                # Show the frame
+                if show_frame == True:
+                    if frame_name == "colour frame": cv2.imshow(f"{colour_name} {frame_name}", masked_image)
+                    else: cv2.imshow(f"{frame_name}", masked_image)
+
+                return mask, masked_image
+            else:
+                print("Incorrect colour value.")
+                return None
+
+            # print(hsv_min)
+            # print(hsv_max)
+
+            # blueMin= (105,80,45)
+            # blueMax= (155,255,230)
+
+    # Detect shapes from given image 
+    def detect_shapes(self, mask, shape_name, frame_name="Shape Frame", show_frame = False, return_largest = False):
+        possible_shapes = {'triangle': 3, 'rectangle': 4, 'star': 10, 'circle': 11}
+
+        if shape_name in possible_shapes:
+            shapes = []
+
+            mask = self.dilating(mask)
+            mask = self.opening(mask)
+
+            mask = self.eroding(mask)
+            mask = self.closing(mask)
+            
+            mask = self.canny_edge_detection(mask)
+            mask = self.dilating(mask)
+
+            contours, h = cv2.findContours(mask, 1, cv2.CHAIN_APPROX_SIMPLE)
+            contours.sort(key = len)
+
+            shape_counter = 0
+
+            if return_largest == False:
+                for contour in contours[-3:]:
+                    #Amount of edges
+                    approx = cv2.approxPolyDP(contour, 0.02*cv2.arcLength(contour, True), True)
+
+                    #Center locations
+                    M = cv2.moments(contour)
+                    if M['m00'] == 0.0:
+                        continue
+                    centroid_x = int(M['m10']/M['m00'])
+                    centroid_y = int(M['m01']/M['m00'])
+
+                    if (len(approx) == possible_shapes.get(shape_name)) or (len(approx) >= 11 and shape_name == 'circle'):
+                        shape = [f"{shape_name} {shape_counter}", contour, centroid_x, centroid_y, len(approx)]
+                        shapes.append(shape)
+                        shape_counter += 1
+
+                return shapes
+            
+            else:
+                # Select the largest one
+                max_contour = max(contours, key = cv2.contourArea)
+
+                #Amount of edges
+                approx = cv2.approxPolyDP(max_contour, 0.02*cv2.arcLength(max_contour, True), True)
+
+                #Center locations
+                M = cv2.moments(max_contour)
+                if M['m00'] != 0.0:
+                    centroid_x = int(M['m10']/M['m00'])
+                    centroid_y = int(M['m01']/M['m00'])
+
+                    if (len(approx) == possible_shapes.get(shape_name)) or (len(approx) >= 11 and shape_name == 'circle'):
+                        shape = [shape_name, max_contour, centroid_x, centroid_y, len(approx)]
+
+                        return shape
+                    
+                    else:
+                        print("No target shapes detected.")
+                        return None
+                else:
+                    print("No contours detected.")
+                    return None
+                
+        else:
+            print("Incorrect shape value.")
+            return None
+    
+    # Closing morphology
+    def closing(self, mask):
+        kernel = np.ones((7,7),np.uint8) 
+        closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        return closing
+
+    # Opening morphology
+    def opening(self, mask):
+        kernel = np.ones((6,6),np.uint8)
+        opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        return opening
+
+    # Blurring morphology
+    def blurring(self, mask):
+        blur = cv2.GaussianBlur(mask,(5,5),0)
+        return blur
+
+    # Eroding morphology
+    def eroding(self, mask):
+        kernel = np.ones((5,5),np.uint8)
+        erosion = cv2.erode(mask, kernel, iterations = 1)
+        return erosion
+
+    # Dilating morphology
+    def dilating(self, mask):
+        kernel = np.ones((5,5),np.uint8)
+        dilation = cv2.dilate(mask, kernel, iterations = 1)
+        return dilation
+
+    # Canny edge detection morphology
+    def canny_edge_detection(self, mask):
+        edges = cv2.Canny(mask,100,200)
+        return edges
+
+    # Creates new csv file and populates it with default values if none exist with the same name
+    def create_hsv_csv_file(self, filename):
+        current_directory = os.getcwd()
+
+        default_hsv_values = {"red": [(172,120,0), (180,255,255)], # red hsv range: low(179,0,0), high(180,255,255) // red,172,120,0,180,255,255
+                            "blue": [(109,116,47),(115,255,100)], # blue hsv range: low(109,116,47), high(118,255,255) // blue,109,116,47,115,255,100
+                            "green":[(65, 61, 0),(95, 255, 255)], # green hsv range: low(65, 61, 0), high(79, 255, 255) // green,65,61,0,95,255,255
+                            "lilac":[(116, 60, 66),(152, 255, 255)],} # lilac hsv range: low(116, 60, 66), high(152, 255, 255) // lilac,116,60,66,152,255,255
+
+        if not os.path.isfile(current_directory + '/' + filename):
+            self.write_hsv_values(filename, default_hsv_values)
+
+    # Read hsv values from file
+    def read_hsv_values(self, filename):
+        self.create_hsv_csv_file(filename) # Create new file if it does not exist
+
+        with open(filename, mode='r') as file:
+            reader = csv.reader(file)
+            hsv_values = {rows[0]:[[int(rows[1]), int(rows[2]), int(rows[3])], [int(rows[4]), int(rows[5]), int(rows[6])]] for rows in reader}
+
+        return hsv_values
+    
+    # Write hsv values  to file
+    def write_hsv_values(self, filename, hsv_values):
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            for colour in hsv_values:
+                writer.writerow([colour, hsv_values[colour][0][0], hsv_values[colour][0][1], hsv_values[colour][0][2],
+                                         hsv_values[colour][1][0], hsv_values[colour][1][1], hsv_values[colour][1][2]])
+    
+    # Change hsv values from file
+    def change_hsv_values(self, colour, parameter, range, value):
+        self.current_hsv_values = self.read_hsv_values(self.hsv_filename)
+
+        colours = ["red", "blue", "green", "lilac"]
+        parameters = ["hue", "saturation", "value"]
+        ranges = ["low", "high"]
+
+        if colour in colours and parameter in parameters and range in ranges:
+            self.current_hsv_values[colour][ranges.index(range)][parameters.index(parameter)] = value
+            self.write_hsv_values(self.hsv_filename, self.current_hsv_values)
+        else:
+            print("Invalid input")
